0638a036d43d6e56133b2a47a78c577623d64aa0
samira rostami zadeh <samirarostami71@gmail.com>
2022-11-10 12:26:40 +0330

    initial project with 2 page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Profile from './Profile';
+import Order from './Order';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+      <main className="order-pages">
+        <Profile />
+        <Order />
+      </main>
+    )
+};
+
 
 export default App;
